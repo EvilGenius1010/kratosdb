dd567bba4507f0345b7e8c77fa4a8aa99a981ca8
EvilGenius1010 <kolhatkarh@gmail.com>
2025-07-03 18:51:16 +0000

    added .datadb folder

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,27 @@ mod constants;
 mod errors;
 mod parser;
 mod storage;
+use core::error::Error;
+
 use clap::Parser;
 use parser::*;
 
 fn main() {
     let cli = CLICommands::parse();
-    println!("{:?}", cli.command);
-    // match &cli.command{
-    //     // get =>
+
+    println!("{:?} running...", cli.command);
+
+    get_item("man".to_string());
+
+    // match cli.command {
+    //     Commands::get { value } => get_item(value),
+    //     Commands::set { key, value } => {
+    //         println!("In progress!");
+    //         Ok(Result<(),Error>);
+    //     }
+    //     Commands::del { key } => {
+    //         println!("In progress!");
+    //         Ok(Result<(),Error>);
+    //     }
     // }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,15 +1,42 @@
+use core::error::Error;
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read},
+};
+
 use clap::{Parser, Subcommand};
 
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
+#[command(author="hk", version="1.001", about, long_about = None)]
 pub struct CLICommands {
     #[command(subcommand)]
     pub command: Commands,
 }
+
+// apply correct linting rule to disable warning.
+//#[warn(non_camel_case_types)]
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
     get { value: String },
     set { key: String, value: String },
     del { key: String },
+}
+
+//key can be dropped after read.
+pub fn get_item(key: String) -> Result<(), Error> {
+    // read .dbdata/primindex which is gonna be an index
+
+    //propagate
+    let read_index = File::open(".dbdata/.primindex")?;
+    //why mut?
+    let mut index_buf_reader = BufReader::new(read_index);
+
+    // space separated numbers containing the indexes from where the indexes start.
+    let mut keystartindices: Vec<u32> = Vec::new();
+
+    for tokens in index_buf_reader.bytes() {
+        println!("{:?}", tokens);
+    }
+    Ok(())
 }
